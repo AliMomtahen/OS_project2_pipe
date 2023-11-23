11a110fb8aad3734c053c7c1f252900cba68c561
Your Name <you@example.com>
2023-11-23 23:32:25 +0330

    add func creat...

--- master.cpp
+++ master.cpp
@@ -37,6 +37,15 @@ vector<string> find_builds(string building_dir){
     return build_lst;
 }
 
+int   creat_proc(vector<string> &build_lst ,vector<int>& build_pid_lst ,string building_dir){
+    for(int i=0;i<build_lst.size() ; i++){
+        cout << build_lst[i] << " ";
+    }
+
+    cout << endl;
+}
+
+
 
 int main(int argc , const char *argv[]){
     if(argc < 2){
@@ -45,6 +54,9 @@ int main(int argc , const char *argv[]){
     }
     string building_dir = (string)  argv[1];
     vector<string> build_lst = find_builds(building_dir);
+    vector<int> build_pid_lst(build_lst.size());
+    int office_pid=-1;
+    creat_proc(build_lst , build_pid_lst , building_dir);
     
 
     
