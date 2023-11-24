428a924b5f94f0c230af8766ee185c6053475d60
Your Name <you@example.com>
2023-11-24 11:45:07 +0330

    add build cpp file

---
+++ building.cpp
@@ -0,0 +1,24 @@
+#ifndef _BUILDING_CPP
+#define _BUILDING_CPP
+
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <vector>
+#include <strings.h>
+#include <dirent.h>
+#include <sstream>
+#include <fstream>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int main(){
+
+    std::cout << "in child\n";
+
+    return 0;
+}
+
+
+#endif
--- master.cpp
+++ master.cpp
@@ -18,6 +18,18 @@
 using namespace std;
 
 
+#define Red_COLOR "\033[0;31m"
+#define Green_COLOR "\033[0;32m"
+#define Yellow_COLOR "\033[0;33m"
+#define Blue_COLOR "\033[0;34m"
+#define RESET_COLOR "\033[0m"
+
+const string BIULD_EXE = "./building.out";
+
+void color_rst(){
+    cout << RESET_COLOR;
+}
+
 vector<string> find_builds(string building_dir){
     vector<string> build_lst;
     DIR *dir;
@@ -25,24 +37,56 @@ vector<string> find_builds(string building_dir){
 
     if ((dir = opendir (building_dir.c_str())) != NULL) {
         while ((ent = readdir (dir)) != NULL) {
-            if (ent->d_type == DT_DIR) {
+            if (ent->d_type == DT_DIR && ((string) ent->d_name) != "." && 
+                ((string )ent->d_name) != "..") {
+            
+
                 build_lst.push_back((string) ent->d_name);
             }
         }
         closedir (dir);
     } else {
-        printf("directory %s not exist\n");
+        cout << "directory "  << building_dir << "not exist\n";
        
     }
     return build_lst;
 }
 
+int creat_one_procces(int &wp){
+    int p[2];
+    if (pipe(p) == -1) {
+        return -1;
+    }
+    int pid = fork();
+    if (pid == 0) {
+        dup2(p[0], STDIN_FILENO);
+        close(p[0]);
+        execl(BIULD_EXE.c_str() ,  BIULD_EXE.c_str() , NULL);
+        perror("execl");
+    } else if (pid > 0) {
+        close(p[0]);
+        wp = p[1];
+    }else{
+        perror("fork");
+    }
+    return pid;
+}
+
+
 int   creat_proc(vector<string> &build_lst ,vector<int>& build_pid_lst ,string building_dir){
+    cout << "number of buildings is " << Red_COLOR << build_lst.size() << 
+    RESET_COLOR << "\nbuildings: ";
     for(int i=0;i<build_lst.size() ; i++){
         cout << build_lst[i] << " ";
     }
 
+    cout << "\n";
+    for(int i=0;i<build_lst.size() ; i++){
+        
+    }
+
     cout << endl;
+    return -1;
 }
 
 
